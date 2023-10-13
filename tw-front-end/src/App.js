689ce538f0dd45e30ee2c3784c139313c8b0332b
Alfredo Velasco <alfredo0234@gmail.com>
2023-10-13 01:47:01 -0500

    Updated the front end to allow for routes for each file. Once there, they can upload their text

--- tw-front-end/src/App.js
+++ tw-front-end/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import 'react-quill/dist/quill.snow.css';
+import FileSelection from './FileSelection';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 import DocumentQuill from './DocumentQuill';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <DocumentQuill></DocumentQuill>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FileSelection/>}/>
+          <Route path="/:id" element={<DocumentQuill/>}/>
+        </Routes>
+      </Router>
     );
   }
 }
--- tw-front-end/src/DocumentQuill.js
+++ tw-front-end/src/DocumentQuill.js
@@ -1,10 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import ReactQuill from 'react-quill';
+import { useParams } from 'react-router-dom';
 
 
 
 export default function DocumentQuill() {
+    const { id } = useParams();
     const [value, setValue] = useState("");
-    return  <ReactQuill theme="snow" value={value} onChange={setValue} />;
+    // return  <ReactQuill theme="snow" value={value} onChange={setValue} />;
+
+    let handleUpdatingContent = async function(){
+		try{
+			const url = "http://localhost:8000/user_file/" + id + ".json";
+			const response = await fetch(url, {
+				method: "PATCH",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({user_file: {content: value}})
+			});
+
+			if(response.ok) {
+				alert("Successfully saved!");
+			} else {
+				// Handle HTTP error responses
+				const errorData = await response.json();
+				alert("Failed to save: " + errorData.name.join(", "));
+			}
+		} catch (error){
+			alert("COULD NOT SAVE! " + error);
+		}
+	}
+
+
+
+    return(
+    <div>
+        <Button onClick={() => {handleUpdatingContent(); }}>Save</Button>
+        <Form>
+            <Form.Group >
+                <Form.Control className='notepad' onChange={(e) => {setValue(e.target.value)}}>
+                </Form.Control>
+            </Form.Group>
+        </Form>
+    </div>
+    );
 }
